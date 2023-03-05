9af82ff3a2fe90013bceacb6e5d81206ddbaab1a
Vitaliy Chaykin <fierygull@yandex.ru>
2023-03-05 18:01:34 +0500

    Add logger in auth service

--- auth_service/src/main.rs
+++ auth_service/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
 use std::env;
 
 mod db;
@@ -8,11 +8,13 @@ mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let app_port = env::var("AUTH_SERVICE_PORT").expect("AUTH_SERVICE_PORT not found.");
     let app_url = format!("0.0.0.0:{}", &app_port);
     HttpServer::new(|| {
         App::new()
             .configure(users::init_routes)
+            .wrap(Logger::new("%a %r %s %T"))
     })
         .bind(&app_url)?
         .run()
