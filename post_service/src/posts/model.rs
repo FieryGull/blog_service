b3ebeba0c3edf830d3dc619fbb99310f5f1dcc3e
Vitaliy Chaykin <fierygull@yandex.ru>
2023-03-06 22:39:58 +0500

    Use wjt auth in post service

--- post_service/src/posts/model.rs
+++ post_service/src/posts/model.rs
@@ -10,15 +10,15 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 
-#[derive(Serialize, Deserialize, AsChangeset, Insertable)]
+#[derive(Serialize, Deserialize, AsChangeset)]
 #[diesel(table_name = posts_table)]
-pub struct Post {
-    pub user_id: Uuid,
+pub struct NewPost {
     pub title: String,
     pub body: String,
 }
 
-#[derive(Serialize, Deserialize, Queryable)]
+#[derive(Serialize, Deserialize, Queryable, AsChangeset, Insertable)]
+#[diesel(table_name = posts_table)]
 pub struct Posts {
     pub id: Uuid,
     pub user_id: Uuid,
@@ -27,50 +27,58 @@ pub struct Posts {
 }
 
 impl Posts {
-    pub fn find_all() -> Result<Vec<Self>, CustomError> {
+    pub fn find_all(user_id: Uuid) -> Result<Vec<Self>, CustomError> {
         let conn = &mut db::connection()?;
-        let posts = posts_table::table.load::<Posts>(conn)?;
+        let posts = posts_table::table.filter(posts_table::user_id.eq(user_id)).load::<Posts>(conn)?;
         Ok(posts)
     }
 
-    pub fn find(id: Uuid) -> Result<Self, CustomError> {
+    pub fn find(id: Uuid, user_id: Uuid) -> Result<Self, CustomError> {
         let conn = &mut db::connection()?;
-        let post = posts_table::table.filter(posts_table::id.eq(id)).first(conn)?;
+        let post = posts_table::table
+            .filter(posts_table::id.eq(id))
+            .filter(posts_table::user_id.eq(user_id))
+            .first(conn)?;
         Ok(post)
     }
 
-    pub fn create(post: Post) -> Result<Self, CustomError> {
+    pub fn create(user_id: Uuid, post: NewPost) -> Result<Self, CustomError> {
         let conn = &mut db::connection()?;
-        let post = Post::from(post);
+        let post = Posts::from(post, user_id);
         let post = diesel::insert_into(posts_table::table)
             .values(post)
             .get_result(conn)?;
         Ok(post)
     }
 
-    pub fn update(id: Uuid, post: Post) -> Result<Self, CustomError> {
+    pub fn update(id: Uuid, user_id: Uuid, post: NewPost) -> Result<Self, CustomError> {
         let conn = &mut db::connection()?;
         let post = diesel::update(posts_table::table)
             .filter(posts_table::id.eq(id))
+            .filter(posts_table::user_id.eq(user_id))
             .set(post)
             .get_result(conn)?;
         Ok(post)
     }
 
-    pub fn delete(id: Uuid) -> Result<usize, CustomError> {
+    pub fn delete(id: Uuid, user_id: Uuid) -> Result<usize, CustomError> {
         let conn =&mut db::connection()?;
-        let res = diesel::delete(posts_table::table.filter(posts_table::id.eq(id)))
+        let res = diesel::delete(posts_table::table
+            .filter(posts_table::id.eq(id))
+            .filter(posts_table::user_id.eq(user_id))
+        )
             .execute(conn)?;
         Ok(res)
     }
 }
 
-impl Post {
-    fn from(post: Post) -> Post {
-        Post {
-            user_id: post.user_id,
-            title: post.title,
-            body: post.body,
+impl Posts {
+    fn from(NewPost { title, body }: NewPost, user_id: Uuid) -> Self {
+        Posts {
+            id: Uuid::new_v4(),
+            user_id,
+            title,
+            body
         }
     }
 }
--- post_service/src/posts/routes.rs
+++ post_service/src/posts/routes.rs
@@ -1,45 +1,51 @@
 use crate::{
-    posts::{Post, Posts},
+    posts::{NewPost, Posts},
     common_lib::{
         error_handler::CustomError,
-        jwt_auth::JwtMiddleware
+        jwt_auth::JwtMiddleware,
     },
 };
 use actix_web::{delete, get, post, put, web, HttpResponse};
-use serde_json::json;
 use uuid::Uuid;
 
 #[get("/posts")]
-async fn find_all() -> Result<HttpResponse, CustomError> {
-    let posts = web::block(|| Posts::find_all()).await.unwrap();
-    Ok(HttpResponse::Ok().json(posts))
+async fn find_all(user_data: JwtMiddleware) -> Result<HttpResponse, CustomError> {
+    let posts = web::block(move || Posts::find_all(user_data.user_id)).await.unwrap();
+    Ok(HttpResponse::Ok().json(posts.unwrap()))
 }
 
 #[get("/posts/{id}")]
-async fn find(id: web::Path<Uuid>) -> Result<HttpResponse, CustomError> {
-    let post = Posts::find(id.into_inner())?;
+async fn find(id: web::Path<Uuid>,
+              user_data: JwtMiddleware) -> Result<HttpResponse, CustomError> {
+    let post = Posts::find(id.into_inner(), user_data.user_id)?;
     Ok(HttpResponse::Ok().json(post))
 }
 
 #[post("/posts")]
-async fn create(post: web::Json<Post>) -> Result<HttpResponse, CustomError> {
-    let post = Posts::create(post.into_inner())?;
+async fn create(post: web::Json<NewPost>,
+                user_data: JwtMiddleware) -> Result<HttpResponse, CustomError> {
+    let post = Posts::create(user_data.user_id, post.into_inner())?;
     Ok(HttpResponse::Ok().json(post))
 }
 
 #[put("/posts/{id}")]
 async fn update(
     id: web::Path<Uuid>,
-    post: web::Json<Post>,
+    post: web::Json<NewPost>,
+    user_data: JwtMiddleware,
 ) -> Result<HttpResponse, CustomError> {
-    let post = Posts::update(id.into_inner(), post.into_inner())?;
+    let post = Posts::update(id.into_inner(), user_data.user_id, post.into_inner())?;
     Ok(HttpResponse::Ok().json(post))
 }
 
 #[delete("/posts/{id}")]
-async fn delete(id: web::Path<Uuid>) -> Result<HttpResponse, CustomError> {
-    let deleted_post = Posts::delete(id.into_inner())?;
-    Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_post })))
+async fn delete(id: web::Path<Uuid>,
+                user_data: JwtMiddleware) -> Result<HttpResponse, CustomError> {
+    let deleted_post = Posts::delete(id.into_inner(), user_data.user_id)?;
+    match deleted_post {
+        0 => Err(CustomError::new(404, "The query object not found".to_string())),
+        _ => Ok(HttpResponse::Ok().finish()),
+    }
 }
 
 pub fn init_routes(config: &mut web::ServiceConfig) {
